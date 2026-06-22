af84bb2a7bf6da77cb46336a1701b480e4654671
Moises Blanca <moisesblanca@fourptzero.com>
2026-06-23 00:47:09 +0800

    Test

--- main.py
+++ main.py
@@ -3,8 +3,23 @@ from sqlmodel import Session, select
 from db import engine
 from models import Member, SQLModel
 from routes import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# =====================
+# CORS FIX
+# =====================
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 def get_session():
     with Session(engine) as session:
