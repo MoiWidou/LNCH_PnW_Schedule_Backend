e2d7064336191574927b028e3303c1ae263f10e5
Moises Blanca <moisesblanca@fourptzero.com>
2026-06-23 05:37:04 +0800

    Redeploy cors error fix

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://lnhc-pnw-sched.netlify.app/"
+        "https://lnhc-pnw-sched.netlify.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
